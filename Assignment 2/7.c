2e4dc80f85d5c23261ed7a4975a35359e0272b1d
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-03-23 02:48:59 +0530

    Update 7.c

--- Assignment 2/7.c
+++ Assignment 2/7.c
@@ -1,22 +1,29 @@
 #include<stdio.h>
 int main()
-/* converting decimal to base 5 */
 {
-    int num, n[10] = {0};
-    float inp,f;
-    printf("Enter the number \n");
-    scanf("%f",&inp);
-    num = inp;
-    f = inp -num;
-    /* converting integer part */
-    for (int i = 0; num != 0; i++)
-    {
-        n[i] = num%5;
-        num = num/5;
+    /* Coverting the decimal to base 5 */
+    float n;
+    int a,count = 0,b[10]={0},d[10]={0};
+    scanf("%f",&n);
+    a = n;
+    n = n -a ;
+    /* n is the fractional part and a is the integer part */
+    /* Converting integer part */
+    for (int i = 1; a != 0; i++,count++)
+    {   
+        b[10-i] = a%5;
+        a =a /5;
     }
-    for ( int i = 0; i <= 10; i++)
+    /* Eliminating the zeros */
+    int c[count];
+    for (int i = 1; i <= count; i++)
     {
-        printf("%d",n[10-i]);
+        c[i]= b[10-i];
+    }
+    /* Printing the integer part */
+    for (int i = 0; i < count; i++)
+    {   
+        printf("%d",c[count-i]);
     }
     
-}
+    /*Converting fractional part */
