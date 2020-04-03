0bf42d7586492cd850f3b108e5e9b968a917d601
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-03 22:14:53 +0530

    Create 6f.c

---
+++ Assignment 2/6f.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    /*Sum of the series cos(x)*/
+    int n,x;
+    printf("Enter the number of terms \n");
+    scanf("%d",&n);
+    printf("Enter the value of x");
+    scanf("%d",&x);
+    float sum = 0;
+    int count = 2;
+    for(int i = 0; i <2*n;i+=2,count++)
+    {
+        int b = 1;
+        for(int j = i;j>=1;j--)
+        {
+            b = b*j;
+        }
+        sum= sum +(float)(pow(-1,count)*pow(x,i))/(float)b;
+    }
+    printf("%f",sum);
+}
